54c8f81a05bb661b79cfa11e8d7d141cebfa1574
Björn Büttner <Idrinth@users.noreply.github.com>
2017-02-10 12:38:26 +0100

    adding error images

--- Classes/ImageGenerator.php
+++ Classes/ImageGenerator.php
@@ -13,14 +13,30 @@ class ImageGenerator {
         header('Content-Type: image/jpeg');
         $set = json_decode(file_get_contents('config.json'),true)[$set];
         if(!$set) {
-            return;
+            $r = array_keys(json_decode(file_get_contents('config.json'),true));
+            array_unshift($r,'Known Configurations:');
+            return $this->errorImage($r);
         }
         /* 'https://dotd.idrinth.de/static/tier-service/' => tiers.json */
         $data = json_decode(file_get_contents($data),true);
         if(!$data) {
-            return;
+            return $this->errorImage(['Failed to load tier data']);
         }
         imagejpeg($this->getHandler($set['type'])->run($data,$set),null,100);
+    }
+    /**
+     *
+     * @param string[] $lines
+     */
+    protected function errorImage($lines) {
+        $image = imagecreate(600,count($lines) * 20);
+        $white = imagecolorallocate($image,255,255,255);
+        imagefill($image,0,0,$white);
+        $black = imagecolorallocate($image,0,0,0);
+        foreach($lines as $pos => $line) {
+            imagestring($image,2,5,$pos * 20 + 3,$line,$black);
+        }
+        imagejpeg($image);
     }
     /**
      *
